c412a424a6bc763b15f947cfcf95a60344a707d2
Christo du Toit <christo@dutoit.co.uk>
2024-04-15 17:16:04 +0100

    BROKERS: Newtonsoft Serialization Broker - Serialize

---
+++ STX.Serialization.Providers.NewtonsoftJson/Brokers/Serializations/INewtonsoftSerializationBroker.cs
@@ -0,0 +1,18 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace STX.Serialization.Providers.NewtonsoftJson.Brokers.Serializations
+{
+    internal interface INewtonsoftSerializationBroker
+    {
+        ValueTask SerializeAsync<T>(
+            Stream utf8JsonStream,
+            T @object,
+            CancellationToken cancellationToken = default);
+    }
+}
---
+++ STX.Serialization.Providers.NewtonsoftJson/Brokers/Serializations/NewtonsoftSerializationBroker.cs
@@ -0,0 +1,35 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace STX.Serialization.Providers.NewtonsoftJson.Brokers.Serializations
+{
+    internal class NewtonsoftSerializationBroker : INewtonsoftSerializationBroker
+    {
+        private readonly JsonSerializerSettings jsonSerializerSettings;
+
+        public NewtonsoftSerializationBroker(JsonSerializerSettings jsonSerializerSettings) =>
+            this.jsonSerializerSettings = jsonSerializerSettings;
+
+        public async ValueTask SerializeAsync<T>(
+            Stream utf8JsonStream,
+            T @object,
+            CancellationToken cancellationToken = default)
+        {
+            await Task.Run(() =>
+            {
+                using (var writer = new StreamWriter(utf8JsonStream))
+                using (var jsonWriter = new JsonTextWriter(writer))
+                {
+                    var serializer = JsonSerializer.Create(jsonSerializerSettings);
+                    serializer.Serialize(writer, @object);
+                }
+            }, cancellationToken);
+        }
+    }
+}
